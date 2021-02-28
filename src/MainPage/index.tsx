741f512c683df158e46da6773e655a5941dca6e8
Clay Bell <clay.bell@etix.com>
2021-02-27 16:22:50 -0800

    progress

--- src/MainPage/index.tsx
+++ src/MainPage/index.tsx
@@ -2,16 +2,20 @@ import {
   Box,
   Collapse,
   Divider,
-  fade,
+  Grid,
+  lighten,
   List,
   ListItem,
   ListItemIcon,
+  ListItemSecondaryAction,
   ListItemText,
   makeStyles,
   Paper,
 } from "@material-ui/core"
 import ExpandLessIcon from "@material-ui/icons/ExpandLess"
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
+import clsx from "clsx"
+import moment from "moment"
 import React from "react"
 import { AccountGroup, accountGroups } from "../accountTypes"
 import { useFetchAccounts } from "../api/accounts"
@@ -19,53 +23,146 @@ import { useFetchCycleDates, useFetchCycleItems } from "../api/cycleItems"
 import CycleNavigation from "../CycleNavigation"
 import { IAccount } from "../db/Account"
 import { ICycleItem } from "../db/CycleItem"
+import { AccountIcon } from "../PlannerPage/AccountIcon"
+import { Currency } from "../PlannerPage/Currency"
 
-const useAccountGroupBoxStyles = makeStyles((theme) => ({
-  root: {
-    marginBottom: theme.spacing(4),
+const useStyles = makeStyles((theme) => ({
+  grid: {
+    maxHeight: `90vh`,
+    marginBottom: 200,
+  },
+  footer: {
+    backgroundColor: theme.palette.primary.main,
+    position: "fixed",
+    bottom: 0,
+    left: 0,
+    width: `100vw`,
+    paddingLeft: theme.spacing(4),
+    paddingRight: theme.spacing(4),
+    color: theme.palette.primary.contrastText,
+    fontSize: 32,
+    "& .right": {
+      textAlign: "right",
+    },
   },
 }))
 
 export function MainPage() {
+  const classes = useStyles()
   const [date, setDate] = React.useState<string | null>(null)
-  const { data: cycleItems } = useFetchCycleItems(date)
 
+  const { data: cycleItems } = useFetchCycleItems(date)
   const { data: cycleDates } = useFetchCycleDates()
+  const { data: accounts } = useFetchAccounts()
+
   React.useEffect(() => {
     if (cycleDates.length > 0 && date === null) {
       setDate(cycleDates[0])
     }
   }, [cycleDates, date])
 
+  // find endDate
+  const endDate: string = cycleDates
+    .filter((x) => moment(x).isAfter(date))
+    .shift()
+
+  // check if current cycle
+  const today = moment()
+  const isCurrentCycle =
+    !today.isBefore(moment(date)) && !today.isAfter(moment(endDate))
+
+  // find previous carryover
+  const startingBalance = accounts.reduce((sum, account) => {
+    const carryOver = account.carryOver.filter((x) => x.date === date)
+    let startingBalance: number = account.currentBalance
+    if (carryOver.length > 0 && !isCurrentCycle) {
+      startingBalance = carryOver.shift().balance
+    }
+    return sum + startingBalance
+  }, 0)
+  const endingBalance = cycleItems
+    .filter((x) => !x.isPaid)
+    .filter(
+      (x) => accounts.filter((y) => y._id === x.payment.account._id).length > 0
+    )
+    .reduce((sum, x) => sum + x.amount, startingBalance)
+
   return date === null ? null : (
     <>
       <CycleNavigation date={date} onChange={(x) => setDate(x)} />
-
-      {accountGroups.map((accountGroup) => (
-        <AccountGroupBox
-          key={accountGroup.label}
-          accountGroup={accountGroup}
-          cycleItems={cycleItems}
-        />
-      ))}
+      <Grid
+        alignContent="flex-start"
+        className={classes.grid}
+        container
+        spacing={2}
+      >
+        {accountGroups.map((accountGroup) => (
+          <Grid item key={accountGroup.label} xs={12} md={6} lg={4}>
+            <AccountGroupBox
+              accountGroup={accountGroup}
+              cycleItems={cycleItems}
+              accounts={accounts}
+              isCurrentCycle={isCurrentCycle}
+              date={date}
+            />
+          </Grid>
+        ))}
+      </Grid>
+      <Box className={classes.footer}>
+        <Grid container>
+          <Grid item xs={6}>
+            <Currency animate value={startingBalance} />
+          </Grid>
+          <Grid item className="right" xs={6}>
+            <Currency animate value={endingBalance} />
+          </Grid>
+        </Grid>
+      </Box>
     </>
   )
 }
 
+const useAccountGroupBoxStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.primary.contrastText,
+    marginBottom: theme.spacing(2),
+  },
+}))
+
 type AccountGroupProps = {
   accountGroup: AccountGroup
+  accounts: IAccount[]
   cycleItems: ICycleItem[]
+  isCurrentCycle: boolean
+  date: string
 }
 
 function AccountGroupBox(props: AccountGroupProps) {
   const classes = useAccountGroupBoxStyles()
-  const { accountGroup, cycleItems } = props
+  const { accountGroup, cycleItems, isCurrentCycle, date } = props
   const [isExpanded, setIsExpanded] = React.useState(false)
-  const { data: unfilteredAccounts } = useFetchAccounts()
 
-  const accounts = unfilteredAccounts.filter((x) =>
+  const accounts = (props.accounts || []).filter((x) =>
     accountGroup.types.includes(x.type)
   )
+
+  // find previous carryover
+  const carryOver = accounts.reduce((sum, x) => {
+    const found = x.carryOver.find((x) => x.date === date)
+    return sum + (found !== undefined ? found.balance : x.currentBalance)
+  }, 0)
+  let startingBalance: number = accounts.reduce(
+    (sum, acc) => sum + acc.currentBalance,
+    0
+  )
+  if (!isCurrentCycle) {
+    startingBalance = carryOver
+  }
+
+  const endingBalance =
+    startingBalance +
+    cycleItems.filter((x) => !x.isPaid).reduce((x, y) => x + y.amount, 0)
 
   return accounts.length === 0 ? null : (
     <Paper elevation={4} className={classes.root}>
@@ -73,9 +170,16 @@ function AccountGroupBox(props: AccountGroupProps) {
         <List>
           <ListItem button onClick={() => setIsExpanded(!isExpanded)}>
             <ListItemIcon>
-              {!isExpanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+              {!isExpanded ? (
+                <ExpandLessIcon style={{ color: "#fff" }} />
+              ) : (
+                <ExpandMoreIcon style={{ color: "#fff" }} />
+              )}
             </ListItemIcon>
             <ListItemText>{accountGroup.label}</ListItemText>
+            <ListItemSecondaryAction>
+              <Currency animate value={endingBalance} />
+            </ListItemSecondaryAction>
           </ListItem>
         </List>
         <Collapse in={isExpanded}>
@@ -85,6 +189,8 @@ function AccountGroupBox(props: AccountGroupProps) {
               key={account._id}
               account={account}
               cycleItems={cycleItems}
+              date={date}
+              isCurrentCycle={isCurrentCycle}
             />
           ))}
         </Collapse>
@@ -95,19 +201,37 @@ function AccountGroupBox(props: AccountGroupProps) {
 
 const useAccountBoxStyles = makeStyles((theme) => ({
   title: {
-    backgroundColor: fade(theme.palette.primary.main, 0.3),
-    paddingLeft: theme.spacing(3),
+    color: theme.palette.text.primary,
+    backgroundColor: lighten(theme.palette.primary.main, 0.67),
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
     paddingTop: theme.spacing(1),
     paddingBottom: theme.spacing(1),
-    fontWeight: "bold",
+    textTransform: "uppercase",
+    "&:nth-of-type(odd)": {
+      backgroundColor: lighten(theme.palette.primary.main, 0.6),
+    },
   },
   item: {
-    paddingLeft: theme.spacing(6),
-    paddingRight: theme.spacing(3),
+    color: theme.palette.text.primary,
+    backgroundColor: theme.palette.background.paper,
+    "&:nth-of-type(odd)": {
+      backgroundColor: lighten(theme.palette.primary.main, 0.93),
+    },
+  },
+
+  left: {
     paddingTop: theme.spacing(1),
     paddingBottom: theme.spacing(1),
-    "&:nth-of-type(odd)": {
-      backgroundColor: fade(theme.palette.primary.main, 0.1),
+    paddingLeft: theme.spacing(2),
+  },
+  right: {
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+    paddingRight: theme.spacing(2),
+    textAlign: "right",
+    "&.total": {
+      borderTop: `2px solid ${lighten(theme.palette.primary.main, 0.5)}`,
     },
   },
 }))
@@ -115,24 +239,67 @@ const useAccountBoxStyles = makeStyles((theme) => ({
 type AccountBoxProps = {
   account: IAccount
   cycleItems: ICycleItem[]
+  date: string
+  isCurrentCycle: boolean
 }
 
 function AccountBox(props: AccountBoxProps) {
   const classes = useAccountBoxStyles()
-  const { account } = props
+  const { account, date, isCurrentCycle } = props
 
   const cycleItems = (props.cycleItems || []).filter(
     (x) => x.payment.account._id === account._id
   )
 
+  // find previous carryover
+  const carryOver = React.useMemo(() => {
+    const found = account.carryOver.find((x) => x.date === date)
+    return found !== undefined ? found.balance : account.currentBalance
+  }, [account.carryOver, account.currentBalance, date])
+  let startingBalance = account.currentBalance
+  if (!isCurrentCycle) {
+    startingBalance = carryOver
+  }
+
+  const endingBalance =
+    startingBalance +
+    cycleItems.filter((x) => !x.isPaid).reduce((x, y) => x + y.amount, 0)
+
   return (
     <>
-      <Box className={classes.title}>{account.name}</Box>
+      <Box className={classes.title}>
+        <Grid container justify="space-between">
+          <Grid item>
+            <AccountIcon account={account} />
+            &nbsp;&nbsp;{account.name}
+          </Grid>
+          <Grid item>
+            <Currency value={startingBalance} />
+          </Grid>
+        </Grid>
+      </Box>
       {cycleItems.map((item) => (
         <Box key={item._id} className={classes.item}>
-          {item.payment.paidTo}
+          <Grid container>
+            <Grid item xs={9} className={classes.left}>
+              {item.payment.paidTo}
+            </Grid>
+            <Grid item xs={3} className={classes.right}>
+              <Currency value={item.amount} />
+            </Grid>
+          </Grid>
         </Box>
       ))}
+      {cycleItems.length === 0 ? null : (
+        <Box className={classes.item}>
+          <Grid container>
+            <Grid item xs={9} className={classes.left}></Grid>
+            <Grid item xs={3} className={clsx("total", classes.right)}>
+              <Currency animate value={endingBalance} />
+            </Grid>
+          </Grid>
+        </Box>
+      )}
     </>
   )
 }
