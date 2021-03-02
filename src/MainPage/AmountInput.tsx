752b974208d6bef154920611d1843d071ad94b2f
Clay Bell <clay.bell@etix.com>
2021-03-01 22:02:49 -0800

    improved number input for iOS

--- src/MainPage/AmountInput.tsx
+++ src/MainPage/AmountInput.tsx
@@ -1,13 +1,28 @@
 /* eslint-disable jsx-a11y/no-autofocus */
-import { makeStyles, Theme } from "@material-ui/core"
+import {
+  Box,
+  Button,
+  Fade,
+  lighten,
+  makeStyles,
+  Theme,
+  useMediaQuery,
+  useTheme,
+} from "@material-ui/core"
+import AddIcon from "@material-ui/icons/Add"
+import RemoveIcon from "@material-ui/icons/Remove"
 import React, { KeyboardEvent, useEffect, useState } from "react"
+import useDebounce from "react-use/lib/useDebounce"
 
 const useStyles = makeStyles((theme: Theme) => ({
+  root: { position: "relative" },
   inputAmount: {
     textAlign: "right",
-    fontFamily: theme.typography.fontFamily,
+    fontFamily: `'Roboto Mono', monospace`,
     fontSize: theme.typography.fontSize,
     width: 80,
+    lineHeight: 1,
+    paddingRight: 0,
     paddingBottom: 0,
     paddingTop: 0,
     backgroundColor: "transparent",
@@ -16,6 +31,22 @@ const useStyles = makeStyles((theme: Theme) => ({
       margin: 0,
     },
     MozAppearance: "textfield",
+    outline: "none",
+    border: 0,
+    "&:focus": {
+      outline: "none",
+      border: 0,
+      backgroundColor: lighten(theme.palette.primary.main, 0.7),
+      borderRadius: 3,
+    },
+  },
+  plusNegative: {
+    position: "absolute",
+    right: 84,
+    fontSize: 12,
+    minWidth: 48,
+    padding: 0,
+    margin: 0,
   },
 }))
 
@@ -31,6 +62,11 @@ export function AmountInput(props: Props) {
   useEffect(() => {
     setValue(centsToDollars(props.value))
   }, [props.value])
+
+  const [isNegative, setIsNegative] = React.useState(false)
+  React.useEffect(() => {
+    setIsNegative(dollarsToCents(value) < 0)
+  }, [value])
 
   function centsToDollars(pennies: number) {
     return (pennies / 100).toFixed(2)
@@ -62,17 +98,69 @@ export function AmountInput(props: Props) {
     setFirstKeyPressed(true)
   }
 
+  function handleToggleNegative() {
+    if (isNegative) {
+      setValue(centsToDollars(Math.abs(dollarsToCents(value))))
+    } else {
+      setValue(centsToDollars(-1 * Math.abs(dollarsToCents(value))))
+    }
+    setClose((x) => x + 1)
+  }
+
+  const [close, setClose] = React.useState(0)
+  useDebounce(
+    () => {
+      if (close > 0) {
+        props.onChange(dollarsToCents(value))
+      }
+    },
+    100,
+    [close, props.onChange]
+  )
+
+  const theme = useTheme()
+  const isXs = useMediaQuery(theme.breakpoints.down("xs"))
+
   return (
-    <input
-      autoFocus
-      className={classes.inputAmount}
-      type="number"
-      pattern="[0-9]+([\.][0-9]+)?"
-      step="0.01"
-      value={value}
-      onChange={(e) => setValue(e.target.value.replace(/[^\d-.]/g, ""))}
-      onBlur={() => props.onChange(dollarsToCents(value))}
-      onKeyUp={handleKeyPress}
-    />
+    <Box className={classes.root}>
+      <Fade in={isXs} unmountOnExit>
+        <Button
+          className={classes.plusNegative}
+          size="small"
+          onClick={handleToggleNegative}
+        >
+          <RemoveIcon fontSize="inherit" /> /
+          <AddIcon fontSize="inherit" />
+        </Button>
+      </Fade>
+      <input
+        className={classes.inputAmount}
+        autoFocus
+        type="text"
+        pattern="[0-9]*"
+        step="0.01"
+        value={value}
+        onChange={(e) => {
+          let str = e.target.value
+          // for ios num pads
+          if (isXs) {
+            str = str.replace(/[^\d-]/g, "")
+            if (str.length > 2) {
+              str = `${str.substr(0, str.length - 2)}.${str.substr(
+                str.length - 2,
+                2
+              )}`
+            }
+          }
+          // for full desktop
+          else {
+            str = str.replace(/[^\d-.]/g, "")
+          }
+          setValue(str)
+        }}
+        onKeyUp={handleKeyPress}
+        onBlur={() => setClose((x) => x + 1)}
+      />
+    </Box>
   )
 }
