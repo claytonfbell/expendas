de95ce368a2c668703aee1eca91822f2d8751cfc
Clay Bell <clay.bell@etix.com>
2024-11-21 17:19:57 -0800

    chore: new address to bind to

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ git pull
 docker build ./ -t expendas
 docker container stop expendas
 docker container rm expendas
-docker run -d -p 10.7.7.63:3000:3000/tcp --name=expendas --restart=unless-stopped --add-host host.docker.internal:host-gateway expendas
+docker run -d -p 10.7.7.12:3000:3000/tcp --name=expendas --restart=unless-stopped --add-host host.docker.internal:host-gateway expendas
 docker image prune -f
