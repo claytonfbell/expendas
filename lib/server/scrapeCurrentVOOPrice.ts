ceffbe093615a11e215a2ce33369c3bff8ec0efa
Clay Bell <clay.bell@etix.com>
2026-03-30 09:14:22 -0700

    fix: scrape from cnbc

--- lib/server/scrapeCurrentVOOPrice.ts
+++ lib/server/scrapeCurrentVOOPrice.ts
@@ -1,10 +1,17 @@
 export async function scrapeCurrentVOOPrice(): Promise<number | null> {
   try {
-    const response = await fetch("https://finance.yahoo.com/quote/VOO/")
+    // const response = await fetch("https://finance.yahoo.com/quote/VOO/")
+    // // <span class="price yf-1ommk34 base" data-testid="qsp-price">582.96</span>
+    // const priceRegex = /data-testid="qsp-price">(.*?)<\/span>/
+
+    const url = "https://www.cnbc.com/quotes/VOO"
+    //<span class="QuoteStrip-lastPrice">585.23</span>
+    const priceRegex = /<span class="QuoteStrip-lastPrice">(.*?)<\/span>/
+
+    const response = await fetch(url)
+
     const html = await response.text()
 
-    // <span class="price yf-1ommk34 base" data-testid="qsp-price">582.96</span>
-    const priceRegex = /data-testid="qsp-price">(.*?)<\/span>/
     const match = html.match(priceRegex)
     const price = match
       ? parseFloat(match[1].trim().replace(/,/g, "")) * 100
