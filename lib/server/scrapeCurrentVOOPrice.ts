63be0b58dd352551b1bb6360635ec589ca05d3cf
Clay Bell <clay.bell@etix.com>
2026-03-30 14:19:26 -0700

    fix: scraping voo price, use the last quote listed (closed)

--- lib/server/scrapeCurrentVOOPrice.ts
+++ lib/server/scrapeCurrentVOOPrice.ts
@@ -3,16 +3,21 @@ export async function scrapeCurrentVOOPrice(): Promise<number | null> {
     // from cnbc (yahoo and fideliety use javascript for anti-scraping)
     const url = "https://www.cnbc.com/quotes/VOO"
     //<span class="QuoteStrip-lastPrice">585.23</span>
-    const priceRegex = /<span class="QuoteStrip-lastPrice">(.*?)<\/span>/
+    const priceRegex = /<span class="QuoteStrip-lastPrice">.*?<\/span>/g
 
     const response = await fetch(url)
     const html = await response.text()
-    const match = html.match(priceRegex)
-    const price = match
-      ? parseFloat(match[1].trim().replace(/,/g, "")) * 100
-      : null
-
-    return price
+    const matches = html.match(priceRegex)
+    if (matches && matches.length > 0) {
+      const regex = /<span class="QuoteStrip-lastPrice">(.*?)<\/span>/
+      const match = matches[matches.length - 1].match(regex)
+      if (match && match.length > 1) {
+        const priceStr = match[1].replace(/,/g, "")
+        const price = parseFloat(priceStr)
+        return price
+      }
+    }
+    return null
   } catch (error) {
     console.error("Error scraping VOO price: ", error)
     return null
