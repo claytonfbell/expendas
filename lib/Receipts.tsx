df6d8730c3499da024b17f2abdd1d20d612735b4
Clay Bell <clay.bell@etix.com>
2026-04-19 23:24:56 -0700

    fix: improved receipts list

--- lib/Receipts.tsx
+++ lib/Receipts.tsx
@@ -1,6 +1,7 @@
 import DownloadIcon from "@mui/icons-material/Download"
 import EditIcon from "@mui/icons-material/Edit"
 import {
+  Chip,
   IconButton,
   Link,
   Stack,
@@ -11,6 +12,7 @@ import {
   Typography,
 } from "@mui/material"
 import moment from "moment"
+import prettyBytes from "pretty-bytes"
 import { useState } from "react"
 import { ReceiptWithIncludes } from "../pages/api/organizations/[id]/receipts"
 import { useFetchReceipts } from "./api/api"
@@ -40,6 +42,7 @@ export function Receipts() {
               <TableCell>Date</TableCell>
               <TableCell>Date Paid</TableCell>
               <TableCell>Account</TableCell>
+              <TableCell>File</TableCell>
               <TableCell align="right">Download</TableCell>
             </TableRow>
           </TableHead>
@@ -60,12 +63,17 @@ export function Receipts() {
                     moment(receipt.datePaid, "YYYY-MM-DD").format("ll")}
                 </TableCell>
                 <TableCell>{receipt.account.name}</TableCell>
-                <TableCell align="right">
-                  <Stack direction="row" justifyContent="flex-end">
-                    <IconButton onClick={() => setReceipt(receipt)}>
-                      <EditIcon />
-                    </IconButton>
+                <TableCell>
+                  <Stack direction="row" spacing={1} alignItems="center">
+                    <Stack>{receipt.organizationCloudFile.name}</Stack>
+                    <Chip
+                      size="small"
+                      label={prettyBytes(
+                        receipt.organizationCloudFile.cloudFile.size
+                      )}
+                    />
                     <IconButton
+                      size="small"
                       component={Link}
                       href={`${rest.baseURL}/organizations/${receipt.account.organizationId}/receipts/${receipt.id}/download`}
                     >
@@ -73,8 +81,36 @@ export function Receipts() {
                     </IconButton>
                   </Stack>
                 </TableCell>
+                <TableCell align="right">
+                  <Stack direction="row" justifyContent="flex-end">
+                    <IconButton
+                      size="small"
+                      onClick={() => setReceipt(receipt)}
+                    >
+                      <EditIcon />
+                    </IconButton>
+                  </Stack>
+                </TableCell>
               </TableRow>
             ))}
+
+            {/* total row */}
+            <TableRow>
+              <TableCell sx={{ fontWeight: "bold" }}>TOTAL</TableCell>
+              <TableCell></TableCell>
+              <TableCell align="right" sx={{ fontWeight: "bold" }}>
+                {formatMoney(
+                  receipts?.reduce(
+                    (total, receipt) => total + receipt.amount,
+                    0
+                  ) ?? 0
+                )}
+              </TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+              <TableCell></TableCell>
+              <TableCell align="right"></TableCell>
+            </TableRow>
           </TableBody>
         </ExpendasTable>
       </Stack>
