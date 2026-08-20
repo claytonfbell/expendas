80c835339c0d6a3235018fc6f5f98062345e6c92
Clay Bell <clay.bell@etix.com>
2026-08-19 21:45:18 -0700

    rename Total Bond to Total Bond Fund for FBND display name

--- src/components/tickerDisplayNames.ts
+++ src/components/tickerDisplayNames.ts
@@ -1,6 +1,6 @@
 export const tickerDisplayNames: Record<string, string> = {
   VOO: "S&P 500",
-  FBND: "Total Bond",
+  FBND: "Total Bond Fund",
   VTIP: "TIPS Fund",
   VB: "Small Cap Index",
   VTI: "Total Market Index",
